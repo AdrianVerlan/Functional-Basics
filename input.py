16c74e6e0aa39d197d36ccd2a36f8ca0ee898ee8
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-03-25 10:59:36 +0200

    Input Types Function HW

---
+++ input.py
@@ -0,0 +1,24 @@
+def inputINT(write):
+   return int(input(write))
+
+m= inputINT("Write down firs integer: ")
+n= inputINT("Write down second integer: ")
+
+print(m+n)
+
+def inputFloat(write):
+   return float(input(write))
+
+p= inputFloat("Write down firs integer: ")
+o= inputFloat("Write down second integer: ")
+
+print(p+o)
+
+
+def inputBoolean(write):
+   return bool(input(write))
+
+i= inputBoolean("Do you have 26 years? ")
+u= inputBoolean("Are you maried? ")
+
+print(i,u)
